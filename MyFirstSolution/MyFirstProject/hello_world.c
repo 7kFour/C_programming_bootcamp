2189c9b3ac5337cf3b7ecd29560fc0d5108093af
Izalith <48108297+7kFour@users.noreply.github.com>
2022-09-13 15:42:24 -0500

    first couple of sections

---
+++ MyFirstSolution/MyFirstProject/hello_world.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+	printf("Hello beautiful world!\n");
+	return 0;
+}
---
+++ Sec_6_Ex1-2-3_basics/Sec_6_Ex1-2-3_basics/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+
+	printf("I'm the prinf function\n");
+
+	// Chall 1 - print first and last name
+	printf("\nChallenge 1\n");
+	printf("First Name: John\nLast Name: Doe\n");
+
+	// Chall 2 - full name, age, a hobby
+	// use a 3 printf() and also use a single printf()
+	printf("\nChallenge 2\n");
+	printf("Full Name: John Doe\n");
+	printf("Age: 26\n");
+	printf("Hobby: Reverse Engineering Games\n");
+
+	printf("\nFull Name: John Doe\nAge: 26\nHobby: Reverse Engineering Games\n");
+
+	// Chall 3 - create isosceles triangle using *
+	// solve with multiple prinft -- and single prinft
+	// ex  *
+	//	  ***
+	//	 *****
+	printf("\nChallenge 3\n");
+	printf("  *\n");
+	printf(" ***\n");
+	printf("*****\n");
+
+	printf("\nChallenge 3 single printf\n");
+	printf("  *  \n *** \n*****\n");
+
+	return 0;
+}
